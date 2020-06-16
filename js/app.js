478e02d390aff7ae0ad684b3839803fa7805e588
Rohald <rohald_89@live.nl>
2020-06-16 22:24:47 +0200

    added all charts improved styling and added todos

--- js/app.js
+++ js/app.js
@@ -20,54 +20,55 @@ banner.addEventListener("click", (e) => {
 });
 
 //==========================
+//==== Global Chart.js =====
+//==========================
+
+Chart.defaults.global.defaultFontFamily = "Roboto";
+Chart.defaults.global.defaultFontSize = 16;
+Chart.defaults.global.defaultFontColor = "#10375C";
+
+//==========================
 //====== Traffic Chart =====
 //==========================
 
 const trafficCanvas = document.getElementById("traffic-chart");
 
-let trafficData = {
-  labels: [
-    "16-22",
-    "23-29",
-    "30-5",
-    "6-12",
-    "13-19",
-    "20-26",
-    "27-3",
-    "4-10",
-    "11-17",
-    "18-24",
-    "25-31",
-  ],
-  datasets: [
-    {
-      data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-      backgroundColor: "rgba(18,119, 129, .4)",
-      borderWidth: 1,
-    },
-  ],
-};
-
-let trafficOptions = {
-  aspectRatio: 2.5,
-  scales: {
-    yAxes: [
+let trafficData = new Chart(trafficCanvas, {
+  type: "line",
+  data: {
+    labels: [
+      "16-22",
+      "23-29",
+      "30-5",
+      "6-12",
+      "13-19",
+      "20-26",
+      "27-3",
+      "4-10",
+      "11-17",
+      "18-24",
+      "25-31",
+    ],
+    datasets: [
       {
-        ticks: {
-          beginAtZero: true,
-        },
+        data: [250, 750, 500, 1150, 1000, 1250, 750, 1350, 900, 1200, 1500],
+        backgroundColor: "rgba(18,118,128,.4)",
       },
     ],
   },
-  legend: {
-    display: false,
+  options: {
+    aspectRatio: 2.8,
+    legend: false,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
   },
-};
-
-let trafficChart = new Chart(trafficCanvas, {
-  type: "line",
-  data: trafficData,
-  options: trafficOptions,
 });
 
 //==========================
@@ -80,24 +81,24 @@ let dailyChart = new Chart(dailyCanvas, {
   type: "bar",
   data: {
     labels: [
+      "Sunday",
       "Monday",
       "Tuesday",
       "Wednesday",
       "Thursday",
       "Friday",
       "Saturday",
-      "Sunday",
     ],
     datasets: [
       {
-        label: "# of Hits",
-        data: [75, 115, 175, 125, 225, 200, 100],
-        backgroundColor: "rgba(18,119, 129, .4)",
-        borderWidth: 1,
+        data: [100, 125, 80, 135, 180, 225, 119],
+        backgroundColor: "rgba(18,118,129,.4)",
       },
     ],
   },
   options: {
+    aspectRatio: 1.9,
+    legend: false,
     scales: {
       yAxes: [
         {
@@ -107,29 +108,63 @@ let dailyChart = new Chart(dailyCanvas, {
         },
       ],
     },
+  },
+});
+
+//==========================
+//====== Mobile Chart ======
+//==========================
+
+const mobileCanvas = document.getElementById("mobile-chart");
+
+let mobileChart = new Chart(mobileCanvas, {
+  type: "doughnut",
+  data: {
+    labels: ["Phone", "Tablet", "Desktop"],
+    datasets: [
+      {
+        data: [45, 30, 15],
+        backgroundColor: ["#F3C623", "#127681", "#E5E9F8"],
+        borderWidth: 5,
+        borderColor: "#F4F6FF",
+        hoverBorderWidth: 1,
+        hoverBorderColor: "#E5E9F8",
+      },
+    ],
+  },
+  options: {
+    title: {
+      fontSize: 24,
+    },
     legend: {
-      display: false,
+      position: "right",
+    },
+    layout: {
+      padding: {
+        right: 50,
+        top: 20,
+        bottom: 20,
+      },
     },
   },
 });
-// let dailyChart = new Chart(dailyCanvas, {
-//   type: "bar",
-//   data: {
-//     labels: [
-//       "Monday",
-//       "Tuesday",
-//       "Wednesday",
-//       "Thursday",
-//       "Friday",
-//       "Saturday",
-//       "Sunday",
-//     ],
-//     dataSets: [
-//       {
-//         data: [75, 115, 175, 125, 225, 200, 100],
-//         backgroundColor: "rgba(18,119, 129, .4)",
-//         borderWidth: 1,
-//       },
-//     ],
-//   },
-// });
+
+//==============================
+//== TODO - Meet Expectations ==
+//==============================
+
+// Include an alert icon in the header with a marker to notify the user of new alerts and messages
+
+// Add a CSS transition or animation to the bell icon when the user hovers over instanceof
+
+//==============================
+//= TODO - Exceed Expectations =
+//==============================
+
+// Display at least 2 notifications alerts icon gets clicked (pop-up window or dropdown menu)
+
+// Traffic chart widget:
+
+//   Includes navigation allowing to switch between hourly, daily, weekly and monthlly chart
+
+//   Hourly, daily, weekly and monthly buttons display a different line chart on click.
